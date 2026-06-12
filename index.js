54ba58ce5d4d70248130b765503e27401d2bced3
Pebbie Nahm <pebbie.nahm@gmail.com>
2026-06-12 15:55:58 -0700

    syntax errors fixed

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ const pool = new Pool({
 // Route: get all records
 app.get("/records", async (req, res) => {
   try {
-	  const result = await pool.query("SELECT * FROM records WHERE user_id = $1 ORDER BY record_time DESC"
-[MY_USER_ID]);
+	  const result = await pool.query(
+  "SELECT * FROM records WHERE user_id = $1 ORDER BY record_time DESC",
+  [MY_USER_ID]
+);
 	  //const result = await pool.query("SELECT * FROM records ORDER BY record_time DESC");
     res.json(result.rows);
   } catch (err) {
@@ -40,8 +42,11 @@ app.get("/records", async (req, res) => {
 app.get("/currentStatus", async (req, res) => {
   try {
     const result = await pool.query(
-	    //  "SELECT action FROM records WHERE user_id = $1 ORDER BY record_time DESC LIMIT 1" [MY_USER_ID]
-	    "SELECT action FROM records ORDER BY record_time DESC LIMIT 1"
+	    const result = await pool.query(
+  "SELECT action FROM records WHERE user_id = $1 ORDER BY record_time DESC LIMIT 1",
+  [MY_USER_ID]
+);
+	    //"SELECT action FROM records ORDER BY record_time DESC LIMIT 1"
     );
 
     if (result.rows.length === 0) {
